0136be913eda9634e587296e328ef5b7b600c204
qiulihong <qiulihong@gmail.com>
2011-10-20 23:18:56 +1100

    imagethumb

--- Symfony/src/Choumei/LooksBundle/Resources/php.php
+++ Symfony/src/Choumei/LooksBundle/Resources/php.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__) . '/../ImageThumb/PhpthumbFactory.php';
+require_once dirname(__FILE__) . '/../ImageThumb/PhpThumbFactory.php';
 
 /**
  * Handle file uploads via XMLHttpRequest
