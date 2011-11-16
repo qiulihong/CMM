e6309f4b40b6420bea19f67012751eb03881c788
qiulihong <qiulihong@gmail.com>
2011-11-17 08:34:22 +1100

    delete some tmp files

--- Symfony/web/bundles/choumeilooks/js/vote.js
+++ Symfony/web/bundles/choumeilooks/js/vote.js
@@ -1,9 +1,9 @@
 $(function(){
 	$('.vote_action').mouseover(function(e){
-		$(this).html('臭美+1');
+		$(this).html('好美+1');
 	});
 	$('.vote_action').mouseout(function(e){
-		$(this).html('臭美');
+		$(this).html('好美');
 	});
 	/*
 	$('.vote_action').click(function(){
